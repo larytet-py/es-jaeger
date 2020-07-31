ffdedb31059de3941f7b1f0818f40a66d4ca693b
larytet <larytet@yahoo.com>
2020-08-01 00:35:17 +0300

    Update

--- find-matches.py
+++ find-matches.py
@@ -14,11 +14,16 @@ for hit in data["hits"]["hits"]:
     spans = hits.get(trace_id, [])
     spans.append(hit)
     hits[trace_id] = spans
+    #print(len(hits[trace_id]))
 
+#print(hits[trace_id])
 min_trace = len(hits[trace_id])
 max_trace = min_trace
-for spans in hits:
-    max_trace = max(max_trace, len(spans))
-    min_trace = min(min_trace, len(spans))
+histogram = {}
+for trace_id in hits:
+    len_spans = len(hits[trace_id])
+    max_trace = max(max_trace, len_spans)
+    min_trace = min(min_trace, len_spans)
+    histogram[len_spans] = histogram.get(len_spans, 0) + 1
 
-print(max_trace, min_trace)
+print(histogram)
