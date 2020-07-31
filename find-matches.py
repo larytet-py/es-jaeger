4db1dd295960e71fa21044ba549e86422708cfab
larytet <larytet@yahoo.com>
2020-08-01 00:20:50 +0300

    Update

--- find-matches.py
+++ find-matches.py
@@ -12,11 +12,11 @@ for hit in data["hits"]["hits"]:
     print(hit)
     hit_source = hit["_source"]
     trace_id = hit_source["traceID"]
-    traces[trace_id] = traces.get(trace_id, []).append(hit)
+    hits[trace_id] = traces.get(hits, []).append(hit)
 
-min = len(traces[trace_id])
+min = len(hits[trace_id])
 max = min
-for spans in traces:
+for spans in hits:
     max = max(max, len(spans))
     min = min(min, len(spans))
 
